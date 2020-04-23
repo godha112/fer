8010d37dc7e98992c9a5c22b7b9b23040fd42f31
godha112 <36445965+godha112@users.noreply.github.com>
2020-04-24 03:07:37 +0530

    Add files via upload

---
+++ testfer.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+emotion_dict = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+model = load_model('fer_model2 (1).h5',compile = False)
+#print(model.summary())
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        cropped_img = cropped_img/255.0
+        prediction = model.predict(cropped_img)
+        cv2.putText(frame, emotion_dict[int(np.argmax(prediction))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
